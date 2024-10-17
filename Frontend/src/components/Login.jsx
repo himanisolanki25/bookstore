a5e753c944dcef9891dd09fbfd05ffff5aa0b70f
himanisolanki25 <120806213+himanisolanki25@users.noreply.github.com>
2024-10-18 01:21:56 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
       email:data.email,
       password:data.password
     }
-    await axios.post("http://localhost:4001/user/login",userInfo)
+    await axios.post("https://bookstore-2-51pn.onrender.com/user/login",userInfo)
     .then((res)=>{
       console.log(res.data)
       if(res.data){
