f92553a54efbf0e0884ca006323ce4b11ba93119
himanisolanki25 <120806213+himanisolanki25@users.noreply.github.com>
2024-10-18 01:21:13 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -12,7 +12,7 @@ function Course() {
   useEffect(()=>{
     const getBook=async()=>{
       try{
-        const res=await axios.get("http://localhost:4001/book")
+        const res=await axios.get("https://bookstore-2-51pn.onrender.com/book")
         console.log(res.data)
         setBook(res.data)
       } catch(error){
